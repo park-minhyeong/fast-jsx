53843056746838acb1663dd178b3b54c0f9527dd
minhyeong <minhyeong.dev@gmail.com>
2024-09-15 04:20:31 +0900

    [Feat] tailwind mode

--- setupPacakge.js
+++ setupPacakge.js
@@ -16,6 +16,7 @@ function copyDirectory(src, dest) {
   for (let entry of entries) {
     const srcPath = path.join(src, entry.name);
     const destPath = path.join(dest, entry.name);
+
     if (entry.isDirectory()) {
       copyDirectory(srcPath, destPath);
     } else {
@@ -26,32 +27,52 @@ function copyDirectory(src, dest) {
 
 function main() {
   const distPath = path.join(__dirname, "dist");
+  const styleCssPath = path.join(__dirname, "src/globals.css");
+  const assetPath = path.join(__dirname, "src", "asset");
+  const packageJsonPath = path.join(__dirname, "package.json");
+  const tailwindConfigPath = path.join(__dirname, "tailwind.config.ts");
+  const readmePath = path.join(__dirname, "docs", "README.md");
+
   if (!fs.existsSync(distPath)) {
     fs.mkdirSync(distPath);
   }
-
-  const sassPath = path.join(__dirname, "src", "_sass");
-  const distSassPath = path.join(distPath, "_sass");
-
-  const packageJsonPath = path.join(__dirname, "package.json");
-  const distPackageJsonPath = path.join(distPath, "package.json");
 
   const source = fs.readFileSync(packageJsonPath).toString("utf-8");
   const sourceObj = JSON.parse(source);
   sourceObj.scripts = {};
   sourceObj.devDependencies = {};
+
   if (sourceObj.main && sourceObj.main.startsWith("/dist/")) {
     sourceObj.main = sourceObj.main.slice(5);
   }
-  // const distAssetPath = path.join(distPath, "asset");
+
+  const distAssetPath = path.join(distPath, "asset");
+  const distStyleCssPath = path.join(distPath, "globals.css");
+  const distPackageJsonPath = path.join(distPath, "package.json");
   const versionFilePath = path.join(distPath, "version.txt");
+  const npmignorePath = path.join(distPath, ".npmignore");
+  const distTailwindConfigPath = path.join(distPath, "tailwind.config.ts");
+  const distReadmePath = path.join(distPath, "README.md");
+
   fs.writeFileSync(
     distPackageJsonPath,
     Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8")
   );
   fs.writeFileSync(versionFilePath, Buffer.from(sourceObj.version, "utf-8"));
-  copyDirectory(sassPath, distSassPath);
-  // copyDirectory(assetPath, distAssetPath);
+
+  if (fs.existsSync(styleCssPath)) {
+    fs.copyFileSync(styleCssPath, distStyleCssPath);
+  }
+  if (fs.existsSync(npmignorePath)) {
+    fs.copyFileSync(npmignorePath, path.join(distPath, ".npmignore"));
+  }
+  if (fs.existsSync(tailwindConfigPath)) {
+    fs.copyFileSync(tailwindConfigPath, distTailwindConfigPath);
+  }
+  if (fs.existsSync(readmePath)) {
+    fs.copyFileSync(readmePath, distReadmePath);
+  }
+  copyDirectory(assetPath, distAssetPath);
 }
 
 main();
