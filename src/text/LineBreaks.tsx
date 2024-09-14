a1a214ae5968a4fcb92228fe8304d47daf1a4495
minhyeong <minhyeong.dev@gmail.com>
2024-09-15 07:33:39 +0900

    [Feat] create LineBreaks

---
+++ src/text/LineBreaks.tsx
@@ -0,0 +1,21 @@
+export function LineBreaks({
+  texts,
+  className,
+}: {
+  texts?: string[] | string;
+  className?: string;
+}) {
+  const strings =
+    typeof texts === "string"
+      ? texts.split("\n")
+      : texts?.map((text) => text.split("\n")).flat();
+  return (
+    <div className={className}>
+      {strings?.map((text, index) => (
+        <div key={index} className="leading-tight">
+          {text.length > 0 ? text : "\u00a0"}
+        </div>
+      ))}
+    </div>
+  );
+}
