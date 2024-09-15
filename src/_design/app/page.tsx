443ceb710df8e720382e73927fc1c428be2ddd78
minhyeong <minhyeong.dev@gmail.com>
2024-09-15 13:15:32 +0900

    [Feat] add npm link

--- src/_design/app/page.tsx
+++ src/_design/app/page.tsx
@@ -2,27 +2,32 @@ import { LineBreaks } from "../../text/LineBreaks";
 import { cn } from "../../util";
 
 export default function RootPage() {
-  const container = { displays: "flex ", sizes: "w-full" };
+  const container = {
+    displays: "flex flex-col gap-y-3.5 lg:flex-row items-center",
+    sizes: "w-full",
+  };
   return (
     <>
       <div className={cn(container)}>
-        <div className="flex flex-col items-center w-1/2">
+        <div className="flex flex-col gap-y-3.5 items-center w-full lg:w-1/2">
           <img
             src="/images/fast-jsx.png"
             width={200}
             className="animate-spin duration-10000"
           />
-          <div className="font-nicomoji text-6xl">FAST JSX</div>
+          <div className="font-nicomoji text-4xl sm:text-6xl">FAST JSX</div>
         </div>
-        <div className="w-1/2 flex flex-col gap-y-3.5 items-start justify-center">
-          <div className="font-nicomoji text-3xl">FAST JSX is...</div>
+        <div className="items-center w-full lg:w-1/2 flex flex-col gap-y-3.5 lg:items-start justify-center">
+          <div className="font-nicomoji text-3xl hidden lg:block">
+            FAST JSX is...
+          </div>
           <LineBreaks
             texts={[
               "It is a modern library for fast JSX component rendering.",
               "It’s built for React and simplifies complex UI structures.",
               "This library boosts both performance and developer productivity.",
             ]}
-            className="text-sm font-mono"
+            className="text-xs sm:text-sm font-mono text-center lg:text-left"
           />
         </div>
       </div>
@@ -44,6 +49,13 @@ function Navigator() {
         onClick={() =>
           window.open("https://github.com/park-minhyeong/fast-jsx")
         }
+        className=" cursor-pointer"
+      />
+      <img
+        src="/images/icons/npm.png"
+        width={36}
+        onClick={() => window.open("https://www.npmjs.com/package/fast-jsx")}
+        className="border-2 border-black rounded-[6px] cursor-pointer"
       />
       <img
         src="/images/icons/linkedin.png"
@@ -51,6 +63,7 @@ function Navigator() {
         onClick={() =>
           window.open("https://www.linkedin.com/in/minhyeong-park-064383250")
         }
+        className=" cursor-pointer"
       />
     </div>
   );
