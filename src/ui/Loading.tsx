ac40aa6f3df955597548d1ac2ff18c83ff08c8c2
minhyeong <minhyeong.dev@gmail.com>
2024-09-17 15:25:24 +0900

    [Fix] fix loading component with brand image

--- src/ui/Loading.tsx
+++ src/ui/Loading.tsx
@@ -5,16 +5,23 @@ function Loading() {
     displays: "flex justify-center items-center",
     sizes: "w-full h-full",
   };
+  const spinner = {
+    displays: "flex justify-center items-center",
+    sjzes: "w-full h-full ",
+    boundaries: "border-[12px] rounded-full  p-4",
+    styles:
+      "border-t-black border-r-gray-500 border-b-gray-300 border-l-gray-100 ",
+    animations: "duration-4000 animate-spin",
+  };
 
   return (
     <div className={cn(container)}>
       <div className="flex flex-col items-center space-y-4">
-        <div className="relative w-28 h-28">
-          <div className="absolute w-full h-full border-[12px] border-t-blue-500 border-r-blue-400 border-b-blue-300 border-l-blue-600 rounded-full animate-spin"></div>
+        <div className="relative w-40 h-40">
+          <div className={cn(spinner)}>
+            <img src="/images/fast-jsx.png" alt="fast-jsx" />
+          </div>
         </div>
-        <p className="text-blue-500 text-2xl font-semibold animate-pulse">
-          Loading...
-        </p>
       </div>
     </div>
   );
