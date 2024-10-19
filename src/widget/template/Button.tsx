77b6e4999969841cb6d1bf5cf35745ed4edc395c
minhyeong <minhyeong.dev@gmail.com>
2024-10-20 00:19:10 +0900

    [Refactor]

--- src/widget/template/Button.tsx
+++ src/widget/template/Button.tsx
@@ -1,26 +1,35 @@
 import { OnClick } from "../../interface/OnClick";
 import { cn } from "../../util";
 
-interface ButtonProps {
+export interface ButtonProps {
   title: string;
   onClick: OnClick;
   option?: {
+    width?: string;
+    height?: string;
     background?: string;
-    text?: string;
+    textColor?: string;
+    font?: string;
+    boundary?: string;
+    pressure?: string;
   };
 }
-export default function Button({ title, onClick, option }: ButtonProps) {
-  const { background, text } = option ?? {};
+
+export default function Button(props: ButtonProps) {
+  const { background, textColor, font, width, height, boundary, pressure } =
+    props.option ?? {};
   const container = {
     displays: "flex justify-center items-center",
     backgrounds: background ?? "bg-black ",
-    texts: text ?? "text-white",
-    fonts: "text-xs",
-    boundaries: "px-2.5 py-0.5 rounded-md",
+    texts: textColor ?? "text-white",
+    fonts: font ?? "text-xs",
+    sizes: width || height ? cn(width, height) : "px-2.5 py-0.5 ",
+    boundaries: boundary ?? "rounded-md",
+    pressures: pressure,
   };
   return (
-    <button onClick={onClick} className={cn(container)}>
-      {title}
+    <button onClick={props.onClick} className={cn(container)}>
+      {props.title}
     </button>
   );
 }
