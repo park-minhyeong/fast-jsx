07714f1af2d943b1664a9611b7a084b4a1fd5202
minhyeong <minhyeong.dev@gmail.com>
2024-10-22 22:42:21 +0900

    [Feat] create Label component

--- src/widget/index.ts
+++ src/widget/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from "./template/Button";
 export { default as Input } from "./template/Input";
+export { default as Label } from "./template/Label";
---
+++ src/widget/template/Label.tsx
@@ -0,0 +1,29 @@
+import { cn } from "../../util";
+
+interface LabelProps {
+  title: string;
+  option?: {
+    width?: string;
+    height?: string;
+    background?: string;
+    textColor?: string;
+    font?: string;
+    boundary?: string;
+    pressure?: string;
+  };
+}
+
+export default function Label(props: LabelProps) {
+  const { background, textColor, font, width, height, boundary, pressure } =
+    props.option ?? {};
+  const container = {
+    displays: "flex justify-center items-center",
+    backgrounds: background ?? "bg-black ",
+    texts: textColor ?? "text-white",
+    fonts: font ?? "text-xs",
+    sizes: width || height ? cn(width, height) : "px-2.5 py-0.5 ",
+    boundaries: boundary ?? "rounded-md",
+    pressures: pressure,
+  };
+  return <div className={cn(container)}>{props.title}</div>;
+}
