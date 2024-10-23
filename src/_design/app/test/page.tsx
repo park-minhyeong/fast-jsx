ee8de03ece299043a94700754448c9436d19f45d
minhyeong <minhyeong.dev@gmail.com>
2024-10-23 20:55:46 +0900

    [Test]

--- src/_design/app/test/page.tsx
+++ src/_design/app/test/page.tsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
+import { Action, Button, Input, Modal, Shelf } from "../../..";
 import Loading from "../../../ui/Loading";
 
 export default function TestPage() {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>();
   return (
-    <div className="flex justify-center items-center min-h-screen w-full">
+    <Action.Show
+      actions={[
+        [
+          true,
+          <Modal
+            key="signIn"
+            titles={{
+              title: "로그인",
+            }}
+            option={{
+              height: "lg",
+              className: "flex flex-col justify-between",
+            }}
+          >
+            <Input state={[username, setUsername]} />
+            <Input
+              state={[password, setPassword]}
+              type="password"
+              onKeyDown={(e) => {}}
+            />
+            <Button
+              title="asdf"
+              onClick={() => {}}
+              option={{
+                height: "h-12",
+              }}
+            />
+          </Modal>,
+        ],
+      ]}
+    >
       <Loading.Spinner />
-    </div>
+    </Action.Show>
   );
 }
