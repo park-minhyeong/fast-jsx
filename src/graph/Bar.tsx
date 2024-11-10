c066cd9dbd5962b52434a7c242f6923637567941
minhyeong <minhyeong.dev@gmail.com>
2024-11-11 01:20:42 +0900

    [Feat] add feature of ```BarOption```

--- src/graph/Bar.tsx
+++ src/graph/Bar.tsx
@@ -2,14 +2,27 @@ import { DataSet } from "../interface/Data";
 import { heightScale } from "../interface/Size";
 import { cn } from "../util";
 
+interface Option {
+  background?: string;
+  display?: string;
+  boundary?: string;
+  width?: string;
+  height?: string;
+  pressure?: string;
+  barStyle?: string;
+}
 interface BarProps {
   dataSet: Omit<DataSet<[string, number]>, "renderItem">;
+  option?: Option;
 }
 
 export default function Bar(props: BarProps) {
+  const { width, height, background, pressure, barStyle } = props.option ?? {};
   const container = {
+    widths: width ?? "w-full",
     displays: "flex justify-between items-end",
-    sizes: "h-80",
+    sizes: height ?? "h-80",
+    pressures: pressure,
   };
   const body = {
     displays: "flex flex-col items-center justify-end ",
@@ -17,7 +30,7 @@ export default function Bar(props: BarProps) {
   };
   const bar = {
     displays: "w-8",
-    backgrounds: "bg-red-500",
+    barStyles: barStyle ?? "bg-red-500",
   };
   return (
     <div className={cn(container)}>
