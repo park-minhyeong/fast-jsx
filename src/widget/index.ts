61694fb6da90adaa7591ddeb622730102221d2f5
minhyeong <minhyeong.dev@gmail.com>
2025-05-25 17:29:04 +0900

    feat: indexing

--- src/widget/index.ts
+++ src/widget/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./template/Button";
 export { default as Input } from "./template/Input";
 export { default as Label } from "./template/Label";
 export { default as Select } from "./template/Select";
+export { default as TextArea } from "./template/TextArea";
