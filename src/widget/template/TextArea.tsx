fda07f3c893d64eaa1ce3a787f5e6596a470d3e2
minhyeong <minhyeong.dev@gmail.com>
2025-05-25 17:27:45 +0900

    feat: create ```TextArea```

---
+++ src/widget/template/TextArea.tsx
@@ -0,0 +1,59 @@
+import { useRef } from "react";
+import { State, StyleOption, Titles } from "../../interface";
+import { cn } from "../../util";
+
+interface InputProps {
+	state: State<string> | State<string | undefined>;
+	placeholder?: string;
+	onKeyDown?: (e: React.KeyboardEvent, value: string) => void;
+	titles?: Titles;
+	option?: StyleOption;
+}
+export default function TextArea(props: InputProps) {
+	const [value, setValue] = props.state;
+	const ref = useRef<HTMLTextAreaElement>(null);
+	const {
+		width,
+		height,
+		pressure,
+		textColor,
+		font,
+		position,
+	} = props.option ?? {};
+	const container = {
+		positions: position ?? "relative",
+		displays: "flex items-center",
+		widths: width ?? "w-full",
+		heights: height ?? "h-32",
+		radius: props.option?.radius ?? "rounded-md",
+		border: props.option?.border ?? "border-2 border-black",
+		fonts: font ?? "text-lg",
+		textColors: textColor ?? "text-black",
+		styles: "overflow-hidden ",
+	};
+	const body = {
+		styles: "border-transparent focus:border-transparent focus:ring-0 resize-none focus:outline-none",
+		pressures: pressure ?? "pl-2.5",
+		sizes: "w-full h-full",
+		background: props.option?.background ?? "bg-white",
+	};
+	const titleBox = {
+		textColors: props.titles?.titleColor ?? "text-black",
+		fonts: props.titles?.size ?? "text-lg",
+	};
+	return (
+		<div className="w-full">
+			{props.titles && <div className={cn(titleBox)}>{props.titles.title}</div>}
+			<div className={cn(container)}>
+				<textarea
+					value={value}
+					placeholder={props.placeholder ?? "입력해주세요."}
+					onChange={(e) => setValue(e.target.value)}
+					className={cn(body)}
+					onKeyDown={(e) => props.onKeyDown && props.onKeyDown(e, value ?? "")}
+					ref={ref}
+				/>
+			</div>
+		</div>
+	);
+}
