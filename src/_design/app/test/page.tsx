b0b78e8ff18d6556553f1328275d15428110e425
minhyeong <minhyeong.dev@gmail.com>
2025-05-26 05:52:23 +0900

    test: test Input with SelectOptionBox

--- src/_design/app/test/page.tsx
+++ src/_design/app/test/page.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DashboardProvider, Input, Select, TextArea } from "../../..";
 
 export default function TestPage() {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>();
+  const [selects, setSelects] = useState<string[]>([]);
+
   return (
     <DashboardProvider.Layout
       header={{
@@ -16,26 +18,34 @@ export default function TestPage() {
         menus: [{ name: "asdf", link: "https://tosel.org" }],
       }}
     >
-      <Input
-        state={[username, setUsername]}
-        option={{
-          border: '',
-          font: 'text-[64px]'
-        }}
-      />
-      {/* <Select.Toggle
-        titles={{
-          title: username
-        }}
-        state={[username, setUsername]}
-        selectOptions={[{
-          value: "a",
-          title: "a",
-        }, {
-          value: "b",
-          title: "b",
-        }]}
-      /> */}
+      <div className="flex">
+        <div></div>
+        <div className="flex flex-col w-full">
+          <Input
+            state={[username, setUsername]}
+            select={{
+              selectOptions: [
+                { value: '12', title: 'asd1234f' },
+                { value: '123', title: 'asd1234f' },
+                { value: '1234', title: 'asd1234f' },
+              ],
+              onClick: (value) => setSelects([...selects, String(value)])
+            }}
+            option={{
+              rightNode: <div className="w-12 h-12 bg-red-500" />
+            }}
+          />
+          <div
+            className="h-12"
+          >{selects.join(', ') ?? 'none'}</div>
+          <TextArea
+            titles={{
+              title: '프로젝트 설명'
+            }}
+            state={[username, setUsername]}
+          />
+        </div>
+      </div>
     </DashboardProvider.Layout>
   );
 }
