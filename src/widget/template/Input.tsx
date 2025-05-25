cbee2c84295f64f49c6a5a24b24840a1001792ef
minhyeong <minhyeong.dev@gmail.com>
2025-05-26 05:52:04 +0900

    feat: apply SelectOptionBox in ```Input``` component

--- src/widget/template/Input.tsx
+++ src/widget/template/Input.tsx
@@ -1,18 +1,27 @@
-import { useEffect, useRef, useState } from "react";
-import { State, StyleOption, Titles } from "../../interface";
+import { MouseEvent, useEffect, useRef, useState } from "react";
+import { OnClick, SelectOption, State, StyleOption, Titles } from "../../interface";
 import { cn } from "../../util";
 import SVG from "../../asset/SVG";
+import SelectOptionBox from "./Select/organism/SelectOptionBox.organism";
+
+interface InputSelect {
+  selectOptions: SelectOption<string>[];
+  onClick?: (e: string | number) => void;
+}
 
 interface InputProps {
   state: State<string> | State<string | undefined>;
   placeholder?: string;
+  onFocus?: OnClick;
   onKeyDown?: (e: React.KeyboardEvent, value: string) => void;
   type?: "text" | "password" | "email" | "number" | "tel" | "url" | "search";
   titles?: Titles;
+  select?: InputSelect;
   option?: StyleOption;
 }
 export default function Input(props: InputProps) {
   const [value, setValue] = props.state;
+  const [isFocus, setIsFocus] = useState<boolean>(false);
   const [isEyeOpen, setIsEyeOpen] = useState<boolean>(false);
   const ref = useRef<HTMLInputElement>(null);
   const {
@@ -35,7 +44,7 @@ export default function Input(props: InputProps) {
   };
   const body = {
     styles: "border-transparent focus:border-transparent focus:ring-0 outline-none",
-    pressures: pressure ?? "pl-2.5",
+    pressures: props.option?.padding ?? "pl-2.5",
     sizes: "w-full h-full",
     fonts: font ?? "text-lg",
     background: props.option?.background ?? "bg-white",
@@ -51,14 +60,16 @@ export default function Input(props: InputProps) {
   }, [isEyeOpen]);
 
   return (
-    <div className="w-full">
+    <div className="w-full relative" onFocus={props.onFocus}>
       {props.titles && <div className={cn(titleBox)}>{props.titles.title}</div>}
       <div className={cn(container)}>
+        {props.option?.leftNode}
         <input
           value={value}
           type={props.type ?? "text"}
           placeholder={props.placeholder ?? "입력해주세요."}
           onChange={(e) => setValue(e.target.value)}
+          onFocus={() => setIsFocus(true)}
           className={cn(body)}
           onKeyDown={(e) => props.onKeyDown && props.onKeyDown(e, value ?? "")}
           ref={ref}
@@ -75,7 +86,16 @@ export default function Input(props: InputProps) {
               className="absolute right-3.75"
             />
           ))}
+        {props.option?.rightNode}
       </div>
+      {props.select && (
+        <SelectOptionBox
+          state={[isFocus, setIsFocus]}
+          selectOptions={props.select.selectOptions}
+          onClick={props.select.onClick}
+          option={props.option}
+        />
+      )}
     </div>
   );
 }
